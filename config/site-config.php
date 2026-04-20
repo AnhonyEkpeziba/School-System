f9661ebd528f06ce26138a6c095fa5c51ac4d2de
Anthony Tamarauseri Ekpeziba <169076884+AnhonyEkpeziba@users.noreply.github.com>
2026-04-20 09:07:38 -0700

    Update site-config.php

--- config/site-config.php
+++ config/site-config.php
@@ -1,15 +1,52 @@
 <?php
-// Site Configuration - Edit these values
 return [
     'site_name' => 'St. Charles Borromeo Minor Seminary',
-    'site_tagline' => 'Forming Future Priests Since 1985',
-    'contact_email' => 'info@stcharlesseminary.edu.ng',
-    'contact_phone' => '+234 803 456 7890',
-    'contact_address' => 'KM 32 East-West Road, Bomadi, Delta State',
-    'demo_mode' => true,
+    'site_tagline' => 'Forming Future Priests in Knowledge and Virtue',
+    'site_logo' => '/assets/images/logo.png',
+    
+    // Contact Information
+    'contact' => [
+        'address' => 'KM 32 East-West Road, Opp. Police Station, Bomadi, Delta State',
+        'phone' => '+234 803 456 7890',
+        'phone_alt' => '+234 802 345 6789',
+        'email' => 'info@stcharlesseminary.edu.ng',
+        'email_admissions' => 'admissions@stcharlesseminary.edu.ng',
+        'hours' => 'Monday - Friday: 8:00 AM - 5:00 PM'
+    ],
+    
+    // Social Media
+    'social' => [
+        'facebook' => 'https://facebook.com/stcharlesseminary',
+        'twitter' => 'https://twitter.com/stcharlesseminary',
+        'instagram' => 'https://instagram.com/stcharlesseminary',
+        'youtube' => 'https://youtube.com/stcharlesseminary'
+    ],
+    
+    // Important Dates
+    'dates' => [
+        'application_opens' => 'January 15, 2025',
+        'application_closes' => 'June 30, 2025',
+        'entrance_exam' => 'July 15, 2025',
+        'resumption_date' => 'September 15, 2025'
+    ],
+    
+    // Fee Structure
+    'fees' => [
+        'jss1' => 150000,
+        'jss2' => 145000,
+        'jss3' => 145000,
+        'ss1' => 160000,
+        'ss2' => 155000,
+        'ss3' => 155000
+    ],
+    
     'colors' => [
         'primary' => '#065f46',
-        'secondary' => '#f59e0b'
-    ]
+        'primary_dark' => '#064e3b',
+        'secondary' => '#f59e0b',
+        'accent' => '#3b82f6'
+    ],
+    
+    'demo_mode' => true
 ];
 ?>
